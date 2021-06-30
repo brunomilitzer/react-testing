6dd129a6b6b70cd2b220b85efe3eda6201d803d9
brunomilitzer <bruno.militzer@est.tech>
2021-06-30 16:54:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Udemy Course
+
+## [Testing React with Jest and Testing Library](https://github.com/brunomilitzer/react-testing)
+
+**_Important Links_**
+1. [JEST Repository](https://github.com/testing-library/jest-dom#tobechecked)
+2. [ROLE Definitions](https://www.w3.org/TR/wai-aria/#role_definitions)
+
+**_Projects_**
+1. [Color Button](https://github.com/brunomilitzer/react-testing/tree/main/color-button)
---
+++ color-button/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { useState } from "react";
+
+function App() {
+  const [ buttonColor, setButtonColor] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
+  const [disabled, setDisabled] = useState(false);
+
+  return (
+    <div className="App">
+      <div>
+        <button
+          style={{ backgroundColor: disabled ? 'gray' : buttonColor }}
+          onClick={() => setButtonColor(newButtonColor)}
+          disabled={disabled}>
+          Change to { newButtonColor }
+        </button>
+        <input
+          type="checkbox"
+          id="disabled-button-checkbox"
+          defaultChecked={disabled}
+          aria-checked={disabled}
+          onChange={(e) => setDisabled(e.target.checked)} />
+        <label htmlFor="disabled-button-checkbox">Disable button</label>
+      </div>
+    </div>
+  );
+}
+
+export default App;
